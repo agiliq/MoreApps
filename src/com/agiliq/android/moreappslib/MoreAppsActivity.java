33511ee32a3bda03578710aa9f6f334a2cd65302
iontech <ikram@agiliq.com>
2013-01-09 15:01:17 +0530

    Add MoreApps Activity.

---
+++ src/com/agiliq/android/moreappslib/MoreAppsActivity.java
@@ -0,0 +1,11 @@
+package com.agiliq.android.moreappslib;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MoreAppsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_apps);
+    }
+}
