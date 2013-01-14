926795ee1bd2882e5154ee98c0f9490b9d5eb358
iontech <ikram@agiliq.com>
2013-01-14 06:47:25 +0530

    Add code to set up the custom GridView.

--- src/com/agiliq/android/moreappslib/MoreAppsActivity.java
+++ src/com/agiliq/android/moreappslib/MoreAppsActivity.java
@@ -2,10 +2,21 @@ package com.agiliq.android.moreappslib;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MoreAppsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mal_activity_more_apps);
+
+        GridItemData[] gridItems = new GridItemData[10];
+        for (int i = 0; i < gridItems.length; i++) {
+            gridItems[i] = new GridItemData(R.drawable.mal_showcased_application_icon_sample, "Sample Name");
+        }
+
+        GridItemAdapter adapter = new GridItemAdapter(this, R.layout.mal_grid_item, gridItems);
+
+        GridView gridView = (GridView) findViewById(R.id.mal_gridView);
+        gridView.setAdapter(adapter);
     }
 }
